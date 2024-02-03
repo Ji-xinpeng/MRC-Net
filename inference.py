3037b7142fce1d7fa24831af9da7725f96a2d3b6
Ji-xinpeng <93025396+Ji-xinpeng@users.noreply.github.com>
2024-02-03 18:17:14 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -7,50 +7,68 @@ from deals.load_dataset import *
 from inference_utils.utils import *
 
 
+class Inference:
+    def __init__(self, checkpoint_path_classify: str,
+                 checkpoint_path_dectet: str,
+                 annot_path: str) -> None:
+        self.model_classify = torch.load(checkpoint_path_classify) 
+        self.model_detect = torch.load(checkpoint_path_dectet)
+        self.frames = []
 
-def load_dataloader(annot_path):
-    input_mean = [.485, .456, .406]
-    input_std = [.229, .224, .225]
-    normalize = GroupNormalize(input_mean, input_std)
+        self.annot_path = annot_path
 
-    scales = [1, .875, .75, .66]
+    def _load_dataloader(self):
+        input_mean = [.485, .456, .406]
+        input_std = [.229, .224, .225]
+        normalize = GroupNormalize(input_mean, input_std)
 
-    trans_test = torchvision.transforms.Compose([
-        GroupScale([224, 224]),
-        Stack(roll=(args.base_model in ['BNInception', 'InceptionV3'])),
-        ToTorchFormatTensor(div=(args.base_model not in ['BNInception', 'InceptionV3'])),
-        normalize])
+        scales = [1, .875, .75, .66]
 
-    temporal_transform_test = torchvision.transforms.Compose([
-        TemporalUniformCrop_val(args.clip_len)
-    ])
+        trans_test = torchvision.transforms.Compose([
+            GroupScale([224, 224]),
+            Stack(roll=(args.base_model in ['BNInception', 'InceptionV3'])),
+            ToTorchFormatTensor(div=(args.base_model not in ['BNInception', 'InceptionV3'])),
+            normalize])
 
-    inference_data = inference_data_video(annot_path, spatial_transform=trans_test, clip_num = 1,
-                                                    temporal_transform=temporal_transform_test)
+        temporal_transform_test = torchvision.transforms.Compose([
+            TemporalUniformCrop_val(args.clip_len)
+        ])
 
-    return inference_data.get_inference_data()
+        inference_data = inference_data_video(self.annot_path, spatial_transform=trans_test, clip_num = 1,
+                                                        temporal_transform=temporal_transform_test)
 
-
-def main():
-    checkpoint_path = '/root/autodl-tmp/MRC-Net/EgoGesture-mobilenetv2/2024-02-02-15-00-40/mobilenetv2classify.pth'
-
-    # 把这个地址换成自己要推理的视频帧所在文件夹的地址
-    annot_path = "/root/autodl-tmp/MRC-Net/data/EgoGesture_annotation"     
-
-    model = torch.load(checkpoint_path) 
-    model.eval()             
+        return inference_data.get_inference_data()
     
-    # 加载处理自己的数据
-    val_dataloader = load_dataloader(annot_path) 
 
-    with torch.no_grad():
-        outputs = model(val_dataloader[0])
-        outputs = outputs.view(1, 1, -1)
-        outputs = F.softmax(outputs, 2)
-        # 找到概率最大值的索引，这就是预测值
-        _, predicted_class = torch.max(outputs, 2)
-        print(f'预测的类别是: {predicted_class[0][0]}')
+    def detect(self, image):
+        pass
+
+    def inference(self):
+        self.model_classify.eval()
+        # 加载处理自己的数据
+        val_dataloader = self._load_dataloader() 
+        with torch.no_grad():
+            outputs = self.model_classify(val_dataloader[0])
+            outputs = outputs.view(1, 1, -1)
+            outputs = F.softmax(outputs, 2)
+            # 找到概率最大值的索引，这就是预测值
+            _, predicted_class = torch.max(outputs, 2)
+            print(f'预测的类别是: {predicted_class[0][0]}')
+        return int(predicted_class[0][0])
 
 
-if __name__ == '__main__':
-    main()
+
+# 获取当前文件路径
+current_file = os.path.abspath(__file__)
+# 获取所在目录路径
+current_directory = os.path.dirname(current_file)
+
+checkpoint_path = current_directory + '/weights/mobilenetv2classify.pth'
+# 把这个地址换成自己要推理的视频帧所在文件夹的地址
+annot_path = "/root/autodl-tmp/MRC-Net/data/EgoGesture_annotation" 
+
+inference = Inference(checkpoint_path_classify=checkpoint_path,
+                      checkpoint_path_dectet=checkpoint_path,
+                      annot_path=annot_path)
+
+result = inference.inference()
