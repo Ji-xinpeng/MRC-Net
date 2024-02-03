4c3a0995977564be4d11999784d21f2d3d3a6d47
Ji-xinpeng <694001039@qq.com>
2024-02-03 21:26:27 +0800

    20240203

--- inference.py
+++ inference.py
@@ -11,19 +11,15 @@ class Inference:
     def __init__(self, checkpoint_path_classify: str,
                  checkpoint_path_dectet: str,
                  annot_path: str) -> None:
-        self.model_classify = torch.load(checkpoint_path_classify) 
-        self.model_detect = torch.load(checkpoint_path_dectet)
+        self.model_classify = torch.load(checkpoint_path_classify, map_location='cpu') 
+        self.model_detect = torch.load(checkpoint_path_dectet, map_location='cpu')
         self.frames = []
-
         self.annot_path = annot_path
 
     def _load_dataloader(self):
         input_mean = [.485, .456, .406]
         input_std = [.229, .224, .225]
         normalize = GroupNormalize(input_mean, input_std)
-
-        scales = [1, .875, .75, .66]
-
         trans_test = torchvision.transforms.Compose([
             GroupScale([224, 224]),
             Stack(roll=(args.base_model in ['BNInception', 'InceptionV3'])),
@@ -47,6 +43,8 @@ class Inference:
         self.model_classify.eval()
         # 加载处理自己的数据
         val_dataloader = self._load_dataloader() 
+        print("模型所在设备:", next(self.model_classify.parameters()).device)
+        print("张量所在设备:", val_dataloader[0].device)
         with torch.no_grad():
             outputs = self.model_classify(val_dataloader[0])
             outputs = outputs.view(1, 1, -1)
@@ -62,8 +60,8 @@ class Inference:
 current_file = os.path.abspath(__file__)
 # 获取所在目录路径
 current_directory = os.path.dirname(current_file)
-
 checkpoint_path = current_directory + '/weights/mobilenetv2classify.pth'
+
 # 把这个地址换成自己要推理的视频帧所在文件夹的地址
 annot_path = current_directory + "/data/EgoGesture_annotation" 
 
