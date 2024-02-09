da1fcb0e39df8ef9e5162a599c65daf9b11a7b12
Ji-xinpeng <694001039@qq.com>
2024-02-09 17:01:15 +0800

    fix some promble

--- others/params.py
+++ others/params.py
@@ -12,7 +12,7 @@ def parse_opts():
     # args for dataloader
     parser.add_argument('--is_train', action="store_true", default=True)
     parser.add_argument('--batch_size', type=int, default=30)
-    parser.add_argument('--num_workers', type=int, default=36)
+    parser.add_argument('--num_workers', type=int, default=24)
     parser.add_argument('--clip_len', type=int, default=8)
 
     # args for preprocessing
@@ -46,7 +46,7 @@ def parse_opts():
     parser.add_argument('--if_need_load_dataset', default=False, type=str)  #
     parser.add_argument('--begin_split', default=0, type=str)  # my_block
     parser.add_argument('--if_get_data_and_label', default=True, type=str)
-    parser.add_argument('--is_detector_classify', default="classify", type=str) # classify detect
+    parser.add_argument('--is_detector_classify', default="detect", type=str) # classify detect
     parser.add_argument('--is_mobile_v3_small', default=True, type=str)
 
     args = parser.parse_args()
@@ -90,7 +90,7 @@ params['learning_rate'] = args.lr
 
 if args.is_detector_classify == "detect":
     if args.is_mobile_v3_small:
-        params['batch_size'] = args.batch_size * 16
+        params['batch_size'] = int(args.batch_size * 14)
         args.base_model = "mobilenetv3_small"
     else:
         params['batch_size'] = args.batch_size * 10
