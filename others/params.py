9227d3351b9f1ea44ce125e3421b577cb685e1b0
Ji-xinpeng <694001039@qq.com>
2024-06-02 14:29:09 +0800

    fix

--- others/params.py
+++ others/params.py
@@ -64,7 +64,7 @@ def parse_opts():
     parser.add_argument('--if_need_load_dataset', default=False, type=str)  #
     parser.add_argument('--begin_split', default=0, type=str)  # my_block
     parser.add_argument('--if_get_data_and_label', default=True, type=str)
-    parser.add_argument('--is_detector_classify', default="classify", type=str) # classify detect
+    parser.add_argument('--is_detector_classify', default="detect", type=str) # classify detect
     parser.add_argument('--is_mobile_v3_small', default=True, type=str)
 
     parser.add_argument('--system_label', type=float, default=[4, 5, 13, 14, 24, 33, 60, 61, 68, 69, 76, 77, 80, 81])
@@ -138,3 +138,5 @@ if args.is_train_for_system:
     args.lr_steps = [10, 20, 30]
     params['epoch_num'] = 40
     
+if args.is_detector_classify == "detect":
+    args.describe = ""
